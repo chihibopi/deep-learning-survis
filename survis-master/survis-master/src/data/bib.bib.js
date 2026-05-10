b5e19127fab7c5fda9b5ad822e6262de05bb6652
chihibopi <chihibopi@gmail.com>
2026-05-10 16:02:44 +0100

    Add files via upload

---
+++ survis-master/survis-master/src/data/bib.bib.js
@@ -0,0 +1,91 @@
+@article{lecun2015deep,
+  author={LeCun, Yann and Bengio, Yoshua and Hinton, Geoffrey},
+  title={Deep Learning},
+  journal={Nature},
+  year={2015},
+  volume={521},
+  number={7553},
+  pages={436--444},
+  doi={10.1038/nature14539},
+  keywords={foundational_architecture, deep_learning, qualitative}
+}
+@inproceedings{krizhevsky2012imagenet,
+  author={Krizhevsky, Alex and Sutskever, Ilya and Hinton, Geoffrey E.},
+  title={ImageNet Classification with Deep Convolutional Neural Networks},
+  booktitle={Advances in Neural Information Processing Systems},
+  year={2012},
+  volume={25},
+  pages={1097--1105},
+  keywords={foundational_architecture, convolutional_networks, quantitative}
+}
+@inproceedings{vaswani2017attention,
+  author={Vaswani, Ashish and Shazeer, Noam and Parmar, Niki and Uszkoreit, Jakob and Jones, Llion and Gomez, Aidan N. and Kaiser, Lukasz and Polosukhin, Illia},
+  title={Attention Is All You Need},
+  booktitle={Advances in Neural Information Processing Systems},
+  year={2017},
+  volume={30},
+  pages={5998--6008},
+  keywords={foundational_architecture, transformer, quantitative}
+}
+@inproceedings{goodfellow2014generative,
+  author={Goodfellow, Ian and Pouget-Abadie, Jean and Mirza, Mehdi and Xu, Bing and Warde-Farley, David and Ozair, Sherjil and Courville, Aaron and Bengio, Yoshua},
+  title={Generative Adversarial Nets},
+  booktitle={Advances in Neural Information Processing Systems},
+  year={2014},
+  volume={27},
+  pages={2672--2680},
+  keywords={generative_model, gan, qualitative}
+}
+@article{mnih2015human,
+  author={Mnih, Volodymyr and Kavukcuoglu, Koray and Silver, David and Rusu, Andrei A. and Veness, Joel and Bellemare, Marc G. and Hassabis, Demis},
+  title={Human-Level Control through Deep Reinforcement Learning},
+  journal={Nature},
+  year={2015},
+  volume={518},
+  number={7540},
+  pages={529--533},
+  doi={10.1038/nature14236},
+  keywords={reinforcement_learning, dqn, quantitative}
+}
+@inproceedings{bordes2013translating,
+  author={Bordes, Antoine and Usunier, Nicolas and Garcia-Duran, Alberto and Weston, Jason and Yakhnenko, Oksana},
+  title={Translating Embeddings for Modeling Multi-Relational Data},
+  booktitle={Advances in Neural Information Processing Systems},
+  year={2013},
+  volume={26},
+  pages={2787--2795},
+  keywords={specialized_representation, knowledge_graph, quantitative}
+}
+@inproceedings{kipf2017semi,
+  author={Kipf, Thomas N. and Welling, Max},
+  title={Semi-Supervised Classification with Graph Convolutional Networks},
+  booktitle={International Conference on Learning Representations},
+  year={2017},
+  keywords={specialized_representation, graph_neural_networks, quantitative}
+}
+@inproceedings{mcmahan2017communication,
+  author={McMahan, Brendan and Moore, Eider and Ramage, Daniel and Hampson, Seth and Aguera y Arcas, Blaise},
+  title={Communication-Efficient Learning of Deep Networks from Decentralized Data},
+  booktitle={Artificial Intelligence and Statistics},
+  year={2017},
+  pages={1273--1282},
+  keywords={specialized_representation, federated_learning, quantitative}
+}
+@inproceedings{devlin2019bert,
+  author={Devlin, Jacob and Chang, Ming-Wei and Lee, Kenton and Toutanova, Kristina},
+  title={BERT: Pre-Training of Deep Bidirectional Transformers for Language Understanding},
+  booktitle={Proceedings of NAACL-HLT},
+  year={2019},
+  pages={4171--4186},
+  doi={10.18653/v1/N19-1423},
+  keywords={pretrained_language_model, bert, quantitative}
+}
+@inproceedings{brown2020language,
+  author={Brown, Tom and Mann, Benjamin and Ryder, Nick and Subbiah, Melanie and Kaplan, Jared D. and Dhariwal, Prafulla and Amodei, Dario},
+  title={Language Models are Few-Shot Learners},
+  booktitle={Advances in Neural Information Processing Systems},
+  year={2020},
+  volume={33},
+  pages={1877--1901},
+  keywords={pretrained_language_model, gpt3, quantitative}
+}
